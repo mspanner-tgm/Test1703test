8ce3c73552f572caaf5779839d4889f01cf69a49
tgm-mspanner <mspanner@student.tgm.ac.at>
2017-03-29 15:49:03 +0200

    3. Mal committet

--- src/michael/Test1703.java
+++ src/michael/Test1703.java
@@ -3,7 +3,7 @@ package michael;
 public class Test1703 {
 
 	public static void main()	{
-		System.out.println("Hallo Michael!");
+		System.out.println("Hallo Michael :)");
 	}
 	
 	public Test1703() {
