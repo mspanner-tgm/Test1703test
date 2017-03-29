02b8e466c3d697e63be474aa93a1498c7d23ba26
tgm-mspanner <mspanner@student.tgm.ac.at>
2017-03-29 15:44:49 +0200

    erstes mal committet

---
+++ src/michael/Test1703.java
@@ -0,0 +1,13 @@
+package michael;
+
+public class Test1703 {
+
+	public static void main()	{
+		System.out.println("Hallo Michael!");
+	}
+	
+	public Test1703() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
